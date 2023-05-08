5d25113c580ae4b48d6786215cc1796b386d262f
Connor Richard Davis <st10068305@vcconnect.edu.za>
2023-05-08 14:44:49 +0200

    Leave create button out of users page for now

--- app/src/pages/admin/users.page.jsx
+++ app/src/pages/admin/users.page.jsx
@@ -73,14 +73,14 @@ const AdminUsersPage = () => {
       <div class="flex flex-col space-y-3 text-black bg-white w-full h-full overflow-hidden rounded p-3">
         <div class="flex items-center justify-between animate-fade-in">
           <div class="cookie text-2xl">Your Users</div>
-          <div class="flex items-center">
+          {/* <div class="flex items-center">
             <div
               onClick={() => {}}
               class="bg-lime-400 hover:bg-lime-300 active:bg-lime-400 transition-all duration-300 ease-in-out px-2 py-1 rounded cursor-pointer text-black"
             >
               Create
             </div>
-          </div>
+          </div> */}
         </div>
         <div class="flex flex-col w-full h-full space-y-2 overflow-y-auto">
           <For
