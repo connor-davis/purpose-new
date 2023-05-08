647502ff9de3f88d452f7f1cd1d6528f2be52b4c
Connor Richard Davis <st10068305@vcconnect.edu.za>
2023-05-08 20:00:08 +0200

    Fix navmenu not closing when profile is clicked

--- app/src/components/navbar.jsx
+++ app/src/components/navbar.jsx
@@ -352,7 +352,10 @@ const Navbar = () => {
               user.data.userType &&
               user.data.userType !== "admin" && (
                 <div
-                  onClick={() => navigate("/profile")}
+                  onClick={() => {
+                    setNavbarShown(!navbarShown());
+                    navigate("/profile");
+                  }}
                   class="flex items-center space-x-2 w-full h-auto px-3 py-2 text-white border-l border-t border-r border-b border-neutral-900 bg-neutral-800 hover:bg-neutral-700 hover:bg-opacity-50 transition-all duration-300 ease-in-out rounded cursor-pointer"
                 >
                   <svg
