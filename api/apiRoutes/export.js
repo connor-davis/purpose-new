73c87729d20eaf18f2a3be2546ae74a3b2285f2e
Connor Richard Davis <st10068305@vcconnect.edu.za>
2023-05-18 13:56:16 +0200

    Make api suited

--- api/apiRoutes/export.js
+++ api/apiRoutes/export.js
@@ -1,8 +1,7 @@
 const { Router } = require('express');
-const adminRoute = require('../utils/adminRoute');
 const { exportAllData } = require('../utils/export');
 const router = Router();
 
-router.get('/', adminRoute, exportAllData);
+router.get('/:userId', exportAllData);
 
 module.exports = router;
